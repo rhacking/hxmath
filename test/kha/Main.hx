f436f89be8eee111de7e4c4a9f494a0a1d8b6121
Roel Hacking <roel.hacking@gmail.com>
2017-05-24 18:39:33 +0200

    Added Kha test code

---
+++ test/kha/Main.hx
@@ -0,0 +1,23 @@
+import hxmath.math.Vector2;
+import hxmath.math.Vector3;
+import hxmath.math.Vector4;
+import hxmath.math.Quaternion;
+
+class Main {
+	public static function main() {
+		var v = new Vector2(1, 0);
+		var w = new kha.math.FastVector3(0, 1);
+		trace(v + w);
+		v.rotate(Math.PI/2, new Vector2(0, 0));
+		trace(v);
+
+		var v3 = new Vector3(1, 0, 0);
+		var w3 = new kha.math.FastVector3(1, 0, 0);
+		trace(v3 * w3);
+		trace(-v3 * w3);
+		trace(v3.rotate(90, new Vector3(0, 1, 0)));
+
+		var q:Quaternion = kha.math.Quaternion.fromAxisAngle(cast new Vector3(0, 1, 0), Math.PI/2);
+		trace(q.rotate(new Vector3(1, 0, 0)));
+	}
+}
